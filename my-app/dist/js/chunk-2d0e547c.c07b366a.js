908c27563cbb7690420feadc4ab8f2bec4967a5f
xueming <810505339@qq.com>
2020-11-10 15:13:55 +0800

    打包

---
+++ my-app/dist/js/chunk-2d0e547c.c07b366a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e547c"],{"948c":function(e,t,n){"use strict";n.r(t);var c=n("7a23"),o=n("6c02");t["default"]=Object(c["defineComponent"])((function(){var e=Object(o["d"])().getRoutes();return console.log(e),function(){return Object(c["createVNode"])("div",null,[Object(c["createTextVNode"])("List")])}}))}}]);
+//# sourceMappingURL=chunk-2d0e547c.c07b366a.js.map
